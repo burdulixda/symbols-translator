2303ecced2c613d715c67a2c4fe247bab92d31a6
George Burduli <burduli94@gmail.com>
2023-09-19 21:25:08 +0400

    browser input implementation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 const http = require("http");
+const fs = require("fs");
+const translator = require("./translator");
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\n");
+  if (req.method === "GET") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+
+    fs.readFile("index.html", "utf8", (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end("Internal Server Error");
+      } else {
+        res.end(data);
+      }
+    });
+  } else if (req.method === "POST") {
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on("end", () => {
+      const htmlString = body;
+      const result = translator(htmlString);
+
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(result, null, 2));
+    });
+  }
 });
 
 server.listen(port, hostname, () => {
